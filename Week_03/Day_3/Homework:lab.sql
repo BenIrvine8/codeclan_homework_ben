fbb0d05c95a3419b29606e644c2f6c376104e411
BenIrvine8 <85495782+BenIrvine8@users.noreply.github.com>
2021-07-08 09:12:18 +0100

    Day_3 Week_02 homework

---
+++ Week_03/Day_3/Homework:lab.sql
@@ -0,0 +1,130 @@
+/*MVP*/
+/*Q1*/
+SELECT 
+COUNT(iban),
+COUNT(local_account_no)
+FROM pay_details
+WHERE iban IS NULL AND local_account_no IS NULL
+/* No */
+
+/*Q2*/
+SELECT 
+	first_name,
+	last_name,
+	country
+FROM employees 
+ORDER BY country, last_name
+NULLS LAST;
+
+/*Q3*/
+
+SELECT *
+FROM employees
+ORDER BY salary DESC
+NULLS LAST
+LIMIT 10;
+
+/*Q4*/
+SELECT
+	first_name,
+	last_name,
+	salary
+FROM employees 
+WHERE country = 'Hungary'
+ORDER BY salary ASC
+LIMIT 1;
+
+/*Q5*/
+SELECT *
+FROM employees 
+WHERE email ILIKE '%yahoo%';
+
+/*Q6*/
+SELECT 
+department AS department_name,
+	COUNT(id) AS employee_count
+FROM employees
+GROUP BY department;
+
+/*Q7*/
+SELECT
+	department,
+	fte_hours,
+	COUNT(id)
+FROM employees 
+GROUP BY fte_hours, department
+ORDER BY department, fte_hours ASC;
+
+/*Q8*/
+SELECT
+pension_enrol,
+COUNT(id)
+FROM employees
+GROUP BY pension_enrol;
+
+/*Q9*/
+SELECT
+MAX(salary)
+FROM employees 
+WHERE fte_hours = 1 AND department = 'Engineering';
+
+/*Q10*/
+SELECT
+	country,
+	COUNT(id) AS num_of_employees,
+	AVG(salary) AS average_salary
+FROM employees 
+GROUP BY (country)
+HAVING COUNT(id) > 30
+ORDER BY average_salary DESC;
+
+
+/*Q11*/
+SELECT 
+	first_name,
+	last_name,
+	fte_hours,
+	salary,
+	fte_hours * salary AS effective_yearly_salary
+FROM employees;
+
+/*Q12*/
+SELECT 
+	e.first_name,
+	e.last_name
+FROM employees AS e LEFT JOIN pay_details AS pd
+	ON e.id = pd.id 
+WHERE pd.local_tax_code IS NULL;
+
+/*Q13*/
+SELECT 
+e.first_name,
+e.last_name,
+e.fte_hours,
+e.salary,
+(48 * 35 * CAST(t.charge_cost AS INT4) - salary) * fte_hours AS expected_profit
+FROM employees AS e LEFT JOIN teams AS t
+ON e.team_id = t.id;
+
+/*Q14*/
+SELECT 
+department,
+COUNT(id) AS no_name
+FROM employees
+WHERE first_name IS NULL 
+GROUP BY department
+HAVING COUNT(id) >= 2
+ORDER BY no_name DESC, department ASC;
+
+/*Q15*/
+
+SELECT 
+COUNT(id) AS name_count
+FROM employees 
+WHERE name_count > 1
+ORDER BY name_count DESC, first_name;
+
+/*Q16*/
+
+SELECT 
+FROM employees 
